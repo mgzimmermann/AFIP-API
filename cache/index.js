18ebc0bb2e661aa552f425fc7c4cedd4edb9ab18
Martin Zimmermann <martin@bee2bee.com.ar>
2020-06-07 14:59:15 -0300

    Uso el key para poder guardar distintos servicios

--- cache/index.js
+++ cache/index.js
@@ -6,19 +6,37 @@ class Cache {
 		this.storePath = path.join('.', 'cache', `${name}`);
 	}
 
-	getItem(key) {
-		let obj = fs.readFileSync(this.storePath, 'utf8');
+	read() {
+		try {
+			let obj = fs.readFileSync(this.storePath, 'utf8');
 
-		if (obj && obj != '') {
-			return JSON.parse(obj);
+			if (obj && obj != '') {
+				return JSON.parse(obj);
+			}
+		} catch(e) {
+			return {}
+		}
+	}
+
+	getItem(key) {
+		const jj = this.read();
+		console.log('CACHE READ', key, jj[key])
+		if (jj[key]) {
+			return jj[key]
 		}
 
-		return false;
+		return false
 	}
 
 	setItem(key, value) {
+		const jj = this.read();
+
 		if (value) {
-			return fs.writeFileSync(this.storePath, JSON.stringify(value));
+
+			jj[key] = value
+			console.log('CACHE STORE', key, jj[key])
+
+			return fs.writeFileSync(this.storePath, JSON.stringify(jj));
 		}
 
 		return false;
